78ceb83d8c1da07b116c2c0548bee837d107540c
AntonErsh <ershov.anton4ik@yandex.ru>
2024-08-24 21:25:27 +0300

    1

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,5 +1,16 @@
 insert_1 = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 insert_2 = []
-
+list_ = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
 number = insert_1[0]
-print(number)
+zero = 0
+
+while zero < len(insert_1):
+    for i in insert_1:
+        for j in list_:
+            for k in list_:
+                if i % j + k == 0:
+                    print(f'{j} + {k}')
+                    continue
+                else:
+                    print('not')
+        zero += 1
