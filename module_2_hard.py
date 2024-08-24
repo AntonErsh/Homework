5dd294f0cb92ef6ada529501af9d7039e76c77b9
AntonErsh <ershov.anton4ik@yandex.ru>
2024-08-24 22:44:38 +0300

    done

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,16 +1,12 @@
 insert_1 = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-insert_2 = []
-list_ = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
-number = insert_1[0]
+result = ''
 zero = 0
-
-while zero < len(insert_1):
-    for i in insert_1:
-        for j in list_:
-            for k in list_:
-                if i % j + k == 0:
-                    print(f'{j} + {k}')
-                    continue
-                else:
-                    print('not')
-        zero += 1
+n = int(input('Введите целое число от 3 до 20 '))
+if n in insert_1:
+    for j in range(1, n):
+        for k in range(j+1, n):
+            if n % (j + k) == 0:
+                result = result + str(j) + str(k)
+    print('Код: ', result)
+else:
+    print('Необходимо ввести целое число в диапазоне от 3 до 20')
