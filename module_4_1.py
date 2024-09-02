3f74fb4eb84f2ad1ac701f2a00c998ec386a3aab
AntonErsh <ershov.anton4ik@yandex.ru>
2024-09-02 21:49:51 +0300

    done

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+import fake_math as fmd
+import true_math as tmd
+
+
+result1 = fmd.divide(14, 4)
+result2 = fmd.divide(14, 0)
+result3 = tmd.divide(55.5, 5)
+result4 = tmd.divide(77, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
