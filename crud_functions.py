ace054d821a0e4e7ff8879b64ed575791202bfa4
AntonErsh <ershov.anton4ik@yandex.ru>
2024-11-29 14:58:48 +0300

    done

--- crud_functions.py
+++ crud_functions.py
@@ -12,8 +12,33 @@ price INT NOT NULL
 )
 ''')
 
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INT PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INT NOT NULL,
+balance INT NOT NULL
+)
+''')
 
-def get_all_products():
+
+def add_user(username: str, email: str, age: int) -> None:
+    cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)',
+                   (username, email, age, 1000))
+    connection.commit()
+
+
+def is_included(username: str) -> bool:
+    check_user = cursor.execute('SELECT * FROM Users WHERE username = ?', (username, )).fetchone()
+    connection.commit()
+    if check_user:
+        return True
+    else:
+        return False
+
+
+def get_all_products() -> list:
     all_list = []
     select_all = cursor.execute('SELECT * FROM Products')
     for i in select_all:
