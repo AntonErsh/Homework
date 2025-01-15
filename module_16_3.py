f2353bab8706671c4372664021f334db9f9c90a1
AntonErsh <ershov.anton4ik@yandex.ru>
2025-01-15 11:11:14 +0300

    done

--- module_16_3.py
+++ module_16_3.py
@@ -18,28 +18,32 @@ async def register_user(username: Annotated[str, Path(min_length=2,
                         age: Annotated[int, Path(ge=18,
                                                  le=90,
                                                  description='Enter your age')]) -> str:
-    new_id = int(max(users, key=int)) + 1
+    new_id = str(int(max(users, key=int)) + 1)
     users.update({new_id: f'Имя: {username}, возраст: {age}'})
     return f'User {new_id} was registered'
 
 
 @app.put('/user/{user_id}/{username}/{age}')
-async def update_user(user_id: Annotated[int, Path(ge=1,
-                                                   le=100,
-                                                   description='Enter User ID')],
+async def update_user(user_id: Annotated[str, Path(description='Enter User ID')],
                       username: Annotated[str, Path(min_length=2,
                                                     max_length=20,
                                                     description='Enter your username')],
                       age: Annotated[int, Path(ge=18,
                                                le=90,
                                                description='Enter your age')]) -> str:
-    users.update({user_id: f'Имя: {username}, возраст: {age}'})
+    try:
+        users.update({user_id: f'Имя: {username}, возраст: {age}'})
+    except KeyError:
+        return f'User {user_id} not found'
+
     return f'The user {user_id} was updated'
 
 
 @app.delete('/user/{user_id}')
-async def delete_user(user_id: Annotated[int, Path(ge=1,
-                                                   le=100,
-                                                   description='Enter User ID')]) -> str:
-    users.pop(user_id)
+async def delete_user(user_id: Annotated[str, Path(description='Enter User ID')]) -> str:
+    try:
+        users.pop(user_id)
+    except KeyError:
+        return f'User {user_id} not found'
+
     return f"The user {user_id} was deleted"
